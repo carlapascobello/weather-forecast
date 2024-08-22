af39278474c89d467fe66b96622e061042fbc769
carlapascobello <maspascobello@su.edu.ph>
2024-08-22 11:18:53 +0800

    Added the search functionality using the API

--- src/script.js
+++ src/script.js
@@ -26,13 +26,28 @@ function currentDateDetails() {
 
 currentDateDetails();
 
+function refreshWeather(response) {
+  let currentTemperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  currentTemperatureElement.innerHTML = temperature;
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "cfdo22bb3e4ct447ead942a949a20eb3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city");
   searchInput = searchInput.value;
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput;
+  searchCity(searchInput);
 }
 
 let searchFormElement = document.querySelector("#search-city-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Barcelona");
